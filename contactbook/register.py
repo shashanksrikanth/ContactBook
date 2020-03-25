e2161bf8bd8581116d32ba589e49cb93f6a3ebc4
shashanksrikanth1 <ssrikan1@mail.depaul.edu>
2020-03-24 23:32:11 -0500

    Fixed import statement for user registration

--- contactbook/register.py
+++ contactbook/register.py
@@ -4,16 +4,17 @@ import click
 import sqlite3
 from sqlite3 import Error
 import os
+from contactbook import database
 
 @click.command()
 @click.option('--username', prompt='Enter username', required=True)
 @click.option('--password', prompt='Enter password', required=True, hide_input=True, confirmation_prompt=True)
 def register_user(username, password):
-    if check_registration(username) is True:
+    if database.check_registration(username) is True:
         click.echo("User is already registered")
     else:
         credentials = (username, password)
-        confirmation = register_user(credentials)
+        confirmation = database.register_user(credentials)
         if confirmation is not None:
             click.echo("User successfully registered!")
         else:
